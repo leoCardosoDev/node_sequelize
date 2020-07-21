bad7c6eb66e5814c00640e6e18e1a5f9cfbc46be
Leonardo Cardoso <leocardosodev@gmail.com>
2020-07-21 04:56:37 -0300

    chore: bootstrap project

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const methodOverride = require('method-override')
+const app = express()
+
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.json())
+app.use(methodOverride('_method'))
+
+app.listen(3000, () => {
+  console.log('Express has been ignite')
+})
