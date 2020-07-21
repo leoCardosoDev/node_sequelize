a6712df2619f47d83d9acbdc1ea5f30ee791a06b
Leonardo Cardoso <leocardosodev@gmail.com>
2020-07-21 16:19:33 -0300

    feat: Course Completed

--- src/controller/events/index.js
+++ src/controller/events/index.js
@@ -2,8 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', require('./find'))
-router.get('/:id', require('./show'))
 router.get('/new', require('./new'))
+router.get('/edit/:id', require('./edit'))
+router.get('/:id', require('./show'))
+router.put('/:id', require('./update'))
+router.delete('/:id', require('./remove'))
 router.post('/', require('./create'))
 
 module.exports = router
