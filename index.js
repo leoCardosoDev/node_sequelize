5972dff55c140c886c37d05beedd065a39a702d6
Leonardo Cardoso <leocardosodev@gmail.com>
2020-07-21 05:12:57 -0300

    chore: add pug template

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
+const path = require('path')
 const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const methodOverride = require('method-override')
+
 const app = express()
 
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({ extended: false}))
 app.use(bodyParser.json())
 app.use(methodOverride('_method'))
+
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'src/view'))
+
+require('./src/index')(app)
 
 app.listen(3000, () => {
   console.log('Express has been ignite')
